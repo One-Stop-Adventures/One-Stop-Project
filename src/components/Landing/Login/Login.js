61b2e089d12633029c11892140f5828c965b697e
Terry Dinh <terrydinh@Terrys-MacBook-Pro.local>
2017-07-25 16:11:33 -0500

    landing

--- src/components/Landing/Login/Login.js
+++ src/components/Landing/Login/Login.js
@@ -4,16 +4,16 @@ import "../Landing.css"
 class Login extends Component {
   render(){
     return (
-      <div className="col-sm-6 col-md-6">
+      <div className="col-xs-12 col-s-12 col-md-6">
         <div className="thumbnail">
           <div className="caption">
             <h1>Login</h1>
-            <form>
-            <input placeholder="Username"/>
+            <form className="navbar-form">
+            <input className="form-control" placeholder="Username"/>
             <br />
-            <input placeholder="Password"/>
+            <input className="form-control" placeholder="Password"/>
             </form>
-            <p><a className="btn btn-default" role="button">Login</a> <a className="btn btn-default" role="button">Sign-Up</a></p>
+            <p><a className="btn btn-default btn-sm" role="button">Login</a> <a className="btn btn-default btn-sm" role="button">Sign-Up</a></p>
           </div>
         </div>
       </div>
