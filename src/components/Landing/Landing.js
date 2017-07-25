39893218f1d80e10a68e6630d2b1a73d01b4e272
hscottharrison <h.scott.harrison@tcu.edu>
2017-07-25 09:15:49 -0500

    added nav1 markup

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react'
 
+import Nav1 from '../Nav1/Nav1'
+
 class Landing extends Component{
   render(){
     return (
       <div>
+        <Nav1 />
         <h1>One Stop Adventures</h1>
       </div>
     )
---
+++ src/components/Nav1/Nav1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav1 = () => {
+  <ul>
+    <li>Logo</li>
+    <li>Shop</li>
+    <li>About</li>
+    <li>Blog</li>
+  </ul>
+}
+
+export default Nav1;
